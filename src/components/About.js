a64f405ee8ca04b2484777476cf7045a2720f806
Taida <samuelgbusinessacc@gmail.com>
2025-10-30 22:25:49 +0300

    adds components

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
+// src/components/About.js
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({ bio, github, linkedin }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {/* ✅ Conditionally render <p> only if bio exists */}
+      {bio ? <p>{bio}</p> : null}
+      <Links github={github} linkedin={linkedin} />
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,31 @@
+// src/components/App.js
 import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+const user = {
+  name: "Liza",
+  city: "New York",
+  color: "firebrick", // ✅ Add this
+  bio: "I made this!",
+  links: {
+    github: "https://github.com/liza",
+    linkedin: "https://www.linkedin.com/in/liza/",
+  },
+};
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      {/* ✅ Pass 'color' prop to Home */}
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About
+        bio={user.bio}
+        github={user.links.github}
+        linkedin={user.links.linkedin}
+      />
     </div>
   );
 }
